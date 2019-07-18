c8450a05d1f07ec9cc72894971b0018199d770a3
elinlarsen <elin_larsen1@hotmail.fr>
2019-07-18 15:06:07 +0200

    changing search bar style

--- src/components/Utils/SearchBar.js
+++ src/components/Utils/SearchBar.js
@@ -2,18 +2,19 @@ import React from 'react'
 import styled from 'styled-components'
 
 const SearchStyle=styled.input`
-width: 30vw; 
+width: 20vw; 
 border : 1px solid #0C214A;
 border-radius : 10px; 
 padding: 1vh;
 font-size : 1.2rem;
-color : #0C214A; 
+color : white; 
 margin: 1vh; 
+background-color : #0C214A
 `
 
 export default function SearchBar(props) {
     return (
         <SearchStyle type="text" onChange={(e) => props.handleChange(e.target.value)} 
-        placeholder="Search an agora by its name." /> 
+        placeholder={props.placeholder} /> 
     )
 }
